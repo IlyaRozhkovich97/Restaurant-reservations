54f0525bdfa1b222bf450c42f99fe1c8e62e1371
i.rozhkovich <rozhkovich.ilya97@gmail.com>
2024-09-09 12:48:58 +0300

    собранный проект 10 фиксация - исправление

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_NAME'),
-        'USER': "postgres",
+        'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT')
